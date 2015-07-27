3c8db1587dfb536594aabf3f6f1371678a4ac1b4
MaxCoraza <demouser@MacBook-Air-3.local>
2015-07-27 12:05:29 -0700

    fixed the handler's url

--- buddyupappengine/main.py
+++ buddyupappengine/main.py
@@ -64,6 +64,6 @@ jinja_environment = jinja2.Environment(loader =
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/titlepage', TitlePageHandler),
+    ('/', TitlePageHandler),
+    ('/homepage', MainHandler),
 ], debug=True)
