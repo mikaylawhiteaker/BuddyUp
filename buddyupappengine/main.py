208d5a8499130a5760fac2ef12ea70f7ccc0e853
MaxCoraza <demouser@MacBook-Air-3.local>
2015-07-27 11:44:15 -0700

    saving

--- buddyupappengine/main.py
+++ buddyupappengine/main.py
@@ -52,5 +52,6 @@ jinja_environment = jinja2.Environment(loader =
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler), ('/titlepage', TitlePageHandler)
+    ('/', MainHandler),
+    ('/titlepage', TitlePageHandler),
 ], debug=True)
