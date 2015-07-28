a6ff488825ee116f5270153a07f2ccb217cfd6c4
MaxCoraza <demouser@MacBook-Air-3.local>
2015-07-27 17:02:38 -0700

    just removed a piece so that it can work

--- buddyupappengine/main.py
+++ buddyupappengine/main.py
@@ -66,7 +66,7 @@ class CreateHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/createform.html')
         self.response.write(template.render({}))
-    def post(self):
+
 
 
 
