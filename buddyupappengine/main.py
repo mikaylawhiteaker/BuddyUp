60280c61320ede1dd68c03db24f36fe84c047394
MaxCoraza <demouser@MacBook-Air-3.local>
2015-07-28 16:11:25 -0700

    added a nav bar

--- buddyupappengine/main.py
+++ buddyupappengine/main.py
@@ -66,7 +66,15 @@ class buddyRequest(ndb.Model):
 class CreateHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/createform.html')
-        self.response.write(template.render({}))
+        user = users.get_current_user()
+        if user:
+            logout =  users.create_logout_url('/')
+            self.response.write(template.render({"logout":logout,
+                                                 "user": user,
+                                                }))
+
+        else:
+            self.response.write("you are signed out")
 
 
     def post(self):
