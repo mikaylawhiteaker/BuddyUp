c1bdcb08994e76d22a1e402e3578eb319dc27e1f
MaxCoraza <demouser@MacBook-Air-3.local>
2015-07-29 15:42:31 -0700

    added css

---
+++ buddyupappengine/JavaScript/buddies.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#logout_url").mouseenter(function(){
+      $("#logout_url").css({"font-size": "150%", "color": "#FA1423"})
+  });
+
+    $("#logout_url").mouseleave(function(){
+      $("#logout_url").css({"font-size": "100%", "color": "white"})
+  });
+
+    $("#logo").mouseenter(function(){
+      $("#logo").css({"font-size": "150%", "color": "#FA1423"})
+  });
+
+    $("#logo").mouseleave(function(){
+      $("#logo").css({"font-size": "100%", "color":"white"})
+  });
+});
