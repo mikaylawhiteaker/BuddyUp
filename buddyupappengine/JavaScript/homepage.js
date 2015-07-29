ca1d8fd602b72a12e3fb71e712c6ec66ba57df12
svelasco7 <demouser@S-MacBook.local>
2015-07-29 10:22:20 -0700

    js on homepage

---
+++ buddyupappengine/JavaScript/homepage.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $("#logo").mouseenter(function(){
+    $("#logo").css({"font-size": "235%", "color": "#FA1423"})
+});
+
+      $("#logo").mouseleave(function(){
+        $("#logo").css({"font-size": "100%", "color":"white"})
+      });
+
+  $("#logout_url").mouseenter(function(){
+    $("#logout_url").css({"font-size": "220%", "color": "#FA1423"})
+  });
+
+      $("#logout_url").mouseleave(function(){
+        $("#logout_url").css({"font-size": "100%", "color": "white"})
+      });
+
+
+
+
+});
