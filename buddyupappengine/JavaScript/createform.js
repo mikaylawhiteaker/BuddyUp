44305c64c0efd1550f9c55d9ddde2c7965ae7b65
svelasco7 <demouser@S-MacBook.local>
2015-07-30 17:41:00 -0700

    added autocomplete maps

--- buddyupappengine/JavaScript/createform.js
+++ buddyupappengine/JavaScript/createform.js
@@ -23,6 +23,27 @@ $(document).ready(function(){
 
         });
 
+        $(function() {
+          var defaultBounds= new google.maps.LatLngBounds(
+            new google.maps.LatLng(-33.8902, 151.1759),
+            new google.maps.LatLng(-33.8474, 151.2631));
+            var input = document.getElementById('searchTextField');
+            var options = {
+              bounds: defaultBounds};
+
+            var input = document.getElementById('pac-input');
+
+            var autocomplete = new google.maps.places.Autocomplete(input, options
+                );
+
+
+
+
+
+          });
+
+
+
 
 
 
